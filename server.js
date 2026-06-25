be601c645ce99e887d25057887a0daebd5773cbf
sunking18 <66726609+sunking18@users.noreply.github.com>
2026-06-25 18:40:53 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3456;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 const DATA_FILE = path.join(__dirname, 'data', 'submissions.json');
 
